08d00150a8f7f5ee75dca974c5b6cb26938a36b5
Diana <dsandura@gmail.com>
2019-05-05 14:11:52 +0200

    Added Hero Details Component

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,12 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Thor' },
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Capitan America' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Ant Man' },
+  { id: 16, name: 'Black Panther' },
+  { id: 17, name: 'Capitan Marvel' },
+  { id: 18, name: 'Black Widow' }
 ];
